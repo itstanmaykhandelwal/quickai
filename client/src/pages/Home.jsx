9de27a2814a32a79330ba70ae0dc53d1b28cfada
Tanmay Khandelwal <tanmaykhandelwal@Tanmays-MacBook-Air.local>
2025-10-15 23:22:27 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import Hero from '../component/Hero'
+
+const Home = () => {
+    return (
+        <>
+            <Navbar/>
+            <Hero/>
+        </>
+    )
+}
+
+export default Home
