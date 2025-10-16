d59a6b870274372b9d14dad20058b30ecb95592a
Tanmay Khandelwal <tanmaykhandelwal@Tanmays-MacBook-Air.local>
2025-10-16 23:28:14 +0530

    new components and sections added

---
+++ client/src/component/Plan.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Plan = () => {
+    return (
+        <div className='max-w-2xl mx-auto z-20 my-30'>
+            <div className="text-center">
+                <div className="text-slate-700 text-[42px]"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Plan
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
 import Hero from '../component/Hero'
+import AiTools from '../component/AiTools'
+import Testimonial from '../component/Testimonial'
 
 const Home = () => {
     return (
         <>
             <Navbar/>
             <Hero/>
+            <AiTools/>
+            <Testimonial/>
         </>
     )
 }
